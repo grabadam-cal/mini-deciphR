20f9384b7b743d9cf92a03d66d483134786f820d
Gabby Rabadam <gabrielle_rabadam@berkeley.edu>
2023-12-15 21:12:56 -0800

    testing socket parallization to reduce exponentially growing memory load of k.reps x k of k.sweep

--- R/iNMF_ksweep.R
+++ R/iNMF_ksweep.R
@@ -24,16 +24,17 @@
 #' @import Matrix
 #' @import matrixStats
 #' @import parallel
+#' @import parallelly
 #' @import cluster
 #' @import stats
 #' @import rlist
 #' @import RANN
-#' @exportd
+#' @export
 
 
 iNMF_ksweep <- function(seurat.object, assay_slot = 'RNA', Type.thresh = 100, Sample.thresh = 10, Batch = TRUE,
                             scale.factor = 10000, log.norm = TRUE, k.min = 2, k.max = 40, n.reps = 20, nn.pt = 0.3,
-                            max.cores = getOption("mc.cores", 2L), output.reps = FALSE, return.scale.data = T){
+                            max.cores = parallelly::availableCores(), output.reps = FALSE, return.scale.data = T){
   if (!"Sample"%in%colnames(seurat.object@meta.data) |
       !"CellType"%in%colnames(seurat.object@meta.data)){
     stop("metadata slot must contain colnames 'Sample' and 'CellType'")
@@ -103,13 +104,20 @@ iNMF_ksweep <- function(seurat.object, assay_slot = 'RNA', Type.thresh = 100, Sa
     h5_chunk_size <- min(1000, minibatch_size)
 
     Liger_list = list()
-    for (k in c(k.min:k.max)){
+    for (k in c(k.min:k.max)){ #because for loop is outside forking step where individual reps are run,
+      #each previous k in Liger_list (with reps.k) is stored and then exported to the forked workers!!!
+      #exponentially increasing memory load. that's why she crashes!!!!!!
       print(paste0('Running K = ', k))
       reps.k = rep(k, n.reps)
       names(reps.k) = paste0("rep", 1:length(reps.k))
-      Liger_list[[paste0("R", k)]] = mcmapply(LIGER.run, K = reps.k, Liger = c(Liger),
+      cl <- makeCluster(max.cores)
+      clusterExport(cl=cl, c('Liger', 'reps.k', 'minibatch_size', 'h5_chunk_size'),
+                    unclass(lsf.str(envir = asNamespace("deciphR"), all = T)),
+                    envir = as.environment(asNamespace("deciphR")))
+      Liger_list[[paste0("R", k)]] = clusterMap(cl, LIGER.run, K = reps.k, Liger = c(Liger),
                                               minibatch_size = minibatch_size, h5_chunk_size = h5_chunk_size,
-                                              SIMPLIFY = F, mc.cores = max.cores)
+                                              SIMPLIFY = F)
+      stopCluster(cl)
       for (i in 1:length(Liger_list[[paste0("R", k)]])){
         colnames(Liger_list[[paste0("R", k)]][[i]][["W"]]) =  paste0("Rep", i, "_", colnames(Liger_list[[paste0("R", k)]][[i]][["W"]]))
         colnames(Liger_list[[paste0("R", k)]][[i]][["H"]]) = paste0("Rep", i, "_", colnames(Liger_list[[paste0("R", k)]][[i]][["H"]]))
@@ -120,6 +128,83 @@ iNMF_ksweep <- function(seurat.object, assay_slot = 'RNA', Type.thresh = 100, Sa
       print(paste0('Done with ', x, ' K = ', k))
     }
 
+    # FIND CONSENSUS RESULTS
+    print(paste0('Finding consensus results'))
+    Consensus.results = mclapply(Liger_list, function(x){
+      W_list = lapply(x, '[[', 'W')
+      W_2norm = rapply(W_list, f = function(y) {apply(y, MARGIN = 2, FUN = function(y){norm(y, type ="2")})},
+                       how = "replace")
+      W_list = rapply(W_list, f = function(y) {t(apply(y, MARGIN = 2, FUN = function(y) {y/norm(y, type = "2")}))},
+                      how = "replace")
+      W.mat = list.rbind(W_list)
+      kmeans_clusts = dim(W.mat)[1]/n.reps
+
+      # find mean distance to nearest neighbors
+      nn.dist = rowMeans(RANN::nn2(as.matrix(W.mat), k = n.reps)$nn.dists[,c(1:round(n.reps*nn.pt))+1])
+      names(nn.dist) = rownames(W.mat)
+
+      # filter outliers
+      min = diff(quantile(nn.dist, probs = c(0.25, 0.75)))*0.5 + quantile(nn.dist, probs = c(0.75))
+      if (sum(nn.dist>min)>0) {
+        W.mat.filt = W.mat[-which(nn.dist>min),]
+      } else {
+        W.mat.filt = W.mat
+      }
+
+      # perform kmeans clustering
+      km.res = kmeans(W.mat.filt, centers = kmeans_clusts, nstart = 100, iter.max = 100)
+
+      # find consensus W (shared gene loadings)
+      W_consensus = matrix(nrow = kmeans_clusts, ncol = ncol(W.mat.filt))
+      for (i in seq(kmeans_clusts)){
+        row.ind = which(km.res$cluster==i)
+        if (length(row.ind) > 1){
+          W_consensus[i,] = colMedians(W.mat.filt[row.ind,])
+        } else W_consensus[i,] = W.mat.filt[row.ind,]
+      }
+      rownames(W_consensus) = paste0("R", kmeans_clusts, "_Program", seq(kmeans_clusts))
+      colnames(W_consensus) = colnames(W.mat.filt)
+
+      # find consensus V (batch-specific gene loadings)
+      V_list = lapply(x, `[[`, "V")
+      for (i in names(V_list)){
+        V_list[[i]] = lapply(V_list[[i]], function(x){t(t(x)*(1/W_2norm[[i]]))})
+      }
+      V.mat = t(list.cbind(lapply(V_list, list.rbind)))
+      if (sum(nn.dist>min)>0) {
+        V.mat.filt = V.mat[-which(nn.dist>min),]
+      } else {
+        V.mat.filt = V.mat
+      }
+      V_consensus = matrix(nrow = kmeans_clusts, ncol = ncol(V.mat.filt))
+      for (i in seq(kmeans_clusts)){
+        row.ind = which(km.res$cluster==i)
+        if (length(row.ind) > 1){
+          V_consensus[i,] = colMedians(V.mat.filt[row.ind,])
+        } else V_consensus[i,] = V.mat.filt[row.ind,]
+      }
+      rownames(V_consensus) = paste0("R", kmeans_clusts, "_Program", seq(kmeans_clusts))
+      colnames(V_consensus) = colnames(V.mat.filt)
+      Batch.info = 1:length(V_list$rep1)
+      names(Batch.info) = sort(names(V_list$rep1))
+      V_consensus = lapply(Batch.info, function(x){
+        V = V_consensus[,((x-1)*length(colnames(W_consensus))+1):(x*length(colnames(W_consensus)))]
+      })
+
+      # Solve for H (activity program expression scores) using consensus W and V initializations
+      H_consensus_list = lapply(Batch.info, function(x){
+        H = solveNNLS(rbind(t(W_consensus) + t(V_consensus[[x]]),
+                            sqrt(5) * t(V_consensus[[x]])), rbind(t(Liger@scale.data[[x]]), matrix(0,
+                                                                                                   dim(W_consensus)[2], dim(Liger@raw.data[[x]])[2])))
+      })
+      H_consensus_list = lapply(H_consensus_list, t)
+      H_consensus = list.rbind(H_consensus_list)
+      rownames(H_consensus) = rownames((lapply(x, `[[`, "H"))[[1]])
+      colnames(H_consensus) = paste0("R", kmeans_clusts, "_Program", seq(kmeans_clusts))
+      consensus_res = list(H = H_consensus, W = W_consensus, V = V_consensus)
+      return(consensus_res)
+    })
+    print(paste0('Done with ', x))
 
     res = list()
     if (return.scale.data){
@@ -132,86 +217,6 @@ iNMF_ksweep <- function(seurat.object, assay_slot = 'RNA', Type.thresh = 100, Sa
     return(res)
   })
   return(res)
-}
-
-conNMF <- function(Liger_list, n.reps, Liger){
-  # FIND CONSENSUS RESULTS
-  print(paste0('Finding consensus results'))
-  Consensus.results = mclapply(Liger_list, function(x){
-    W_list = lapply(x, '[[', 'W')
-    W_2norm = rapply(W_list, f = function(y) {apply(y, MARGIN = 2, FUN = function(y){norm(y, type ="2")})},
-                     how = "replace")
-    W_list = rapply(W_list, f = function(y) {t(apply(y, MARGIN = 2, FUN = function(y) {y/norm(y, type = "2")}))},
-                    how = "replace")
-    W.mat = list.rbind(W_list)
-    kmeans_clusts = dim(W.mat)[1]/n.reps
-
-    # find mean distance to nearest neighbors
-    nn.dist = rowMeans(RANN::nn2(as.matrix(W.mat), k = n.reps)$nn.dists[,c(1:round(n.reps*nn.pt))+1])
-    names(nn.dist) = rownames(W.mat)
-
-    # filter outliers
-    min = diff(quantile(nn.dist, probs = c(0.25, 0.75)))*0.5 + quantile(nn.dist, probs = c(0.75))
-    if (sum(nn.dist>min)>0) {
-      W.mat.filt = W.mat[-which(nn.dist>min),]
-    } else {
-      W.mat.filt = W.mat
-    }
-
-    # perform kmeans clustering
-    km.res = kmeans(W.mat.filt, centers = kmeans_clusts, nstart = 100, iter.max = 100)
-
-    # find consensus W (shared gene loadings)
-    W_consensus = matrix(nrow = kmeans_clusts, ncol = ncol(W.mat.filt))
-    for (i in seq(kmeans_clusts)){
-      row.ind = which(km.res$cluster==i)
-      if (length(row.ind) > 1){
-        W_consensus[i,] = colMedians(W.mat.filt[row.ind,])
-      } else W_consensus[i,] = W.mat.filt[row.ind,]
-    }
-    rownames(W_consensus) = paste0("R", kmeans_clusts, "_Program", seq(kmeans_clusts))
-    colnames(W_consensus) = colnames(W.mat.filt)
-
-    # find consensus V (batch-specific gene loadings)
-    V_list = lapply(x, `[[`, "V")
-    for (i in names(V_list)){
-      V_list[[i]] = lapply(V_list[[i]], function(x){t(t(x)*(1/W_2norm[[i]]))})
-    }
-    V.mat = t(list.cbind(lapply(V_list, list.rbind)))
-    if (sum(nn.dist>min)>0) {
-      V.mat.filt = V.mat[-which(nn.dist>min),]
-    } else {
-      V.mat.filt = V.mat
-    }
-    V_consensus = matrix(nrow = kmeans_clusts, ncol = ncol(V.mat.filt))
-    for (i in seq(kmeans_clusts)){
-      row.ind = which(km.res$cluster==i)
-      if (length(row.ind) > 1){
-        V_consensus[i,] = colMedians(V.mat.filt[row.ind,])
-      } else V_consensus[i,] = V.mat.filt[row.ind,]
-    }
-    rownames(V_consensus) = paste0("R", kmeans_clusts, "_Program", seq(kmeans_clusts))
-    colnames(V_consensus) = colnames(V.mat.filt)
-    Batch.info = 1:length(V_list$rep1)
-    names(Batch.info) = sort(names(V_list$rep1))
-    V_consensus = lapply(Batch.info, function(x){
-      V = V_consensus[,((x-1)*length(colnames(W_consensus))+1):(x*length(colnames(W_consensus)))]
-    })
-
-    # Solve for H (activity program expression scores) using consensus W and V initializations
-    H_consensus_list = lapply(Batch.info, function(x){
-      H = solveNNLS(rbind(t(W_consensus) + t(V_consensus[[x]]),
-                          sqrt(5) * t(V_consensus[[x]])), rbind(t(Liger@scale.data[[x]]), matrix(0,
-                                                                                                 dim(W_consensus)[2], dim(Liger@raw.data[[x]])[2])))
-    })
-    H_consensus_list = lapply(H_consensus_list, t)
-    H_consensus = list.rbind(H_consensus_list)
-    rownames(H_consensus) = rownames((lapply(x, `[[`, "H"))[[1]])
-    colnames(H_consensus) = paste0("R", kmeans_clusts, "_Program", seq(kmeans_clusts))
-    consensus_res = list(H = H_consensus, W = W_consensus, V = V_consensus)
-    return(consensus_res)
-  })
-  print(paste0('Done with ', x))
 }
 
 LIGER.run <- function(K, Liger, minibatch_size, h5_chunk_size, seed = NULL){
