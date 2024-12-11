3d520580c30d31d50c11ceced200a2990e92bfd8
Gabby <74675873+grabadam-cal@users.noreply.github.com>
2024-12-11 14:21:22 -0800

    Delete tests directory

--- tests/testthat/test-phylo_tree_functions.R
+++
@@ -1,5 +0,0 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
-})
-nmf_test <- readRDS('~/Library/CloudStorage/Box-Box/neely.jdm/NMF/results_summary/nmf_ksweep_results_compiled.rds')
-test_phylo = readRDS('~/Library/CloudStorage/Box-Box/neely.jdm/NMF/DECIPHER_outputs/crosslong_phylo_trees.rds')
